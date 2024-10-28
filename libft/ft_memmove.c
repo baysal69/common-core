b0727622c81d344ae5ed7653bf0ef990d3067fea
Wassim Aissi <waissi@c2r8p10.1337.ma>
2024-10-28 20:54:38 +0100

    zok l memmove

---
+++ libft/ft_memmove.c
@@ -0,0 +1,41 @@
+#include "libft.h"
+#include <string.h>
+
+void *ft_memmove (void *dest, const void *src, size_t n)
+{
+	int i = 0;
+	char *s = (unsigned char *)src;
+	char *d = (unsigned char *)dest;
+	if (!d && !s)
+		return NULL;
+	if(d > s)
+	{
+		while (n > 0)
+		{
+			d[i -1] = s[i - 1];
+			n--;
+		}
+	}
+	else
+	{
+		while(s[i] && i  < n)
+		{
+			d[i] = s[i];
+			i++;
+		}
+	
+		d[i] = '\0';
+	}
+	return (void *)d;
+/*
+}
+int main()
+{
+        char s[] = "baysal";
+        char d[6];
+        char f[6];
+        printf("%s\n",(char *) ft_memmove(d,s,8));
+        printf("%s\n",(char *)    memmove(f,s,8));
+}
+*/
+
