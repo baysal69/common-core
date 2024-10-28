908a3d4b4623fb674699c3c0487e982cc7701aa3
Wassim Aissi <waissi@c2r8p10.1337.ma>
2024-10-28 15:11:00 +0100

    memcpy

--- libft/ft_memcpy.c
+++ libft/ft_memcpy.c
@@ -6,8 +6,11 @@ void *ft_memcpy(void *dest, const void *src, size_t n)
 	int i = 0;
 	char *s = (unsigned char *)src;
 	char *d = (unsigned char *)dest;
-
-	while( s[i] && i < n - 1)
+	if(!dest && !src)
+		return NULL;
+	if(dest == src)
+		return (void *)dest;
+	while( s[i] && i < n)
 	{
 		d[i] = s[i];
 		i++;
@@ -19,6 +22,7 @@ int main()
 {
 	char s[] = "baysal";
 	char d[6];
-	printf("%s\n",(char *)ft_memcpy(d,s,6));
-	printf("%s\n", (char *)memcpy(d,s,6));
+	char f[6];
+	printf("%s\n",(char *) ft_memcpy(d,s,8));
+	printf("%s\n",(char *)    memcpy(f,s,8));
 }
