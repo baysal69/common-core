ead5c26c560f434eda7d38ed80505c99f682dfd7
li7wak <baysal@DESKTOP-H9FSBQ7.>
2024-11-02 16:47:35 +0100

    zbi

--- libft/ft_strtrim.c
+++ libft/ft_strtrim.c
@@ -6,14 +6,22 @@ char *ft_strtrim(char const *s1, char const *set)
 	char *s = (char *)set;
 	char *r;
 	int start = 0;
-	int end = ft_strlen(str);
-	if (!s1 || !s2)
+	int end = ft_strlen(str) - 1;
+	if (!s1 || !set)
 		return NULL;
+	while (ft_strchr(set, s1[start]) && start <= end)
+		start++;
+	if (start > end)
+		return (ft_strdup(s1 + end + 1));
+	while (ft_strchr(set ,s1[end] && end >= 0));
+		end --;
+	r = malloc(end - start + 2);
+	return str + start;
 
 }
 int main()
 {
 	char ch[] = "-- baysal";
-	char chh[] = "baysal";
+	char chh[] = "--";
 	printf("%s",ft_strtrim(ch,chh));
 }
