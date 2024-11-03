d760fe16b47b3b1b1d611f0436751fd9ca1f9a66
li7wak <baysal@DESKTOP-H9FSBQ7.>
2024-11-04 00:36:47 +0100

    zboba

--- libft/ft_strtrim.c
+++ libft/ft_strtrim.c
@@ -1,27 +1,51 @@
 #include "libft.h"
 
-char *ft_strtrim(char const *s1, char const *set)
+static int	ft_check_set(char const c, char const *set)
 {
-	char *str = (char *)s1;
-	char *s = (char *)set;
-	char *r;
-	int start = 0;
-	int end = ft_strlen(str) - 1;
-	if (!s1 || !set)
-		return NULL;
-	while (ft_strchr(set, s1[start]) && start <= end)
-		start++;
-	if (start > end)
-		return (ft_strdup(s1 + end + 1));
-	while (ft_strchr(set ,s1[end] && end >= 0));
-		end --;
-	r = malloc(end - start + 2);
-	return str + start;
+	int	i;
 
+	i = 0;
+	while (set[i] != '\0')
+	{
+		if (set[i] == c)
+			return (1);
+		i++;
+	}
+	return (0);
 }
+
+char	*ft_strtrim(char const *s1, char const *set)
+{
+	size_t	size;
+	char	*new;
+
+	if (!s1 || !set)
+		return (NULL);
+	while (s1)
+	{
+		if (ft_check_set(((char)*s1), set) == 1)
+			s1++;
+		else
+			break ;
+	}
+	size = ft_strlen(s1);
+	while (size != 0)
+	{
+		if (ft_check_set(s1[size - 1], set) == 1)
+			size--;
+		else
+			break ;
+	}
+	new = (char *)malloc(size * sizeof(char) + 1);
+	if (!new)
+		return (NULL);
+	ft_strlcpy(new, (char *)s1, size + 1);
+	return (new);
+}
+/*
 int main()
 {
-	char ch[] = "-- baysal";
-	char chh[] = "--";
+	char ch[] = "---baysal---";
+	char chh[] = "---";
 	printf("%s",ft_strtrim(ch,chh));
-}
+}*/
