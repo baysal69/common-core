d58d90df5853c6d38fd7b0fa5049edc4b5fd674a
baysal <baysal@DESKTOP-H9FSBQ7.>
2024-11-08 03:46:46 +0100

    split done

--- norms_mgadin/ft_split.c
+++ norms_mgadin/ft_split.c
@@ -1,81 +1,92 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_split.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: waissi <waissi@student.1337.ma>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/08 03:44:36 by waissi            #+#    #+#             */
+/*   Updated: 2024/11/08 03:45:20 by waissi           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "libft.h"
 
-static size_t   ft_countword(char const *s, char c)
+static size_t	ft_countword(char const *s, char c)
 {
-    size_t  count;
+	size_t	count;
 
-    if (!*s)
-        return (0);
-    count = 0;
-    while (*s)
-    {
-        while (*s == c)
-            s++;
-        if (*s)
-            count++;
-        while (*s != c && *s)
-            s++;
-    }
-    return (count);
+	if (!*s)
+		return (0);
+	count = 0;
+	while (*s)
+	{
+		while (*s == c)
+			s++;
+		if (*s)
+			count++;
+		while (*s != c && *s)
+			s++;
+	}
+	return (count);
 }
 
-static void    free_array(char **arr, int i)
+static void	free_array(char **arr, int i)
 {
-    if (arr)
-    {
-    while (i >= 0)
-    {
-        free(arr[i]);
-        i--;   
-    }
-    free(arr);
-    }
+	if (arr)
+	{
+		while (i >= 0)
+		{
+			free(arr[i]);
+			i--;
+		}
+		free(arr);
+	}
 }
 
-
-
-// split_words - Helper function to split the string into words
-char *split_words(char const *s, char c, char **lst)
+char	*split_words(char const *s, char c, char **lst)
 {
-    size_t  word_len;
-    int     i = 0;
+	size_t	word_len;
+	int		i;
 
-    while (*s)
-    {
-        while (*s == c && *s) // Skip delimiters
-            s++;
-        if (*s)
-        {
-            word_len = 0;
-            while (s[word_len] && s[word_len] != c)
-                word_len++;
-            lst[i] = ft_substr(s, 0, word_len); // Extract the word
-            if (!lst[i]) // Handle memory allocation failure
-                return (NULL);
-            i++;
-            s += word_len; // Move past the current word
-        }
-    }
-    lst[i] = NULL; // Null-terminate the list of words
-    return (lst);
+	i = 0;
+	while (*s)
+	{
+		while (*s == c && *s)
+			s++;
+		if (*s)
+		{
+			word_len = 0;
+			while (s[word_len] && s[word_len] != c)
+				word_len++;
+			lst[i] = ft_substr(s, 0, word_len);
+			if (!lst[i])
+				return (NULL);
+			i++;
+			s += word_len;
+		}
+	}
+	lst[i] = NULL;
+	return (lst);
 }
-char **ft_split(char const *s, char c)
-{
-    char    **lst;
-    size_t  word_count;
 
-    if (!s)
-        return (NULL);
-    word_count = ft_countword(s, c);
-    lst = (char **)malloc((word_count + 1) * sizeof(char *));
-    if (!lst)
-        return (NULL);
-    if (split_words(s, c, lst) == NULL)
-    {
-        free(lst);
-        return (NULL);
-    }
-    return (lst);
+char	**ft_split(char const *s, char c)
+{
+	char	**lst;
+	size_t	word_count;
+
+	if (!s)
+		return (NULL);
+	word_count = ft_countword(s, c);
+	lst = (char **)malloc((word_count + 1) * sizeof(char *));
+	if (!lst)
+		return (NULL);
+	if (split_words(s, c, lst) == NULL)
+	{
+		free(lst);
+		return (NULL);
+	}
+	return (lst);
 }
 
 // int main()
