af5b55f8744783f5e0871f8e5d5b3a05855208ed
baysal <baysal@DESKTOP-H9FSBQ7.>
2025-03-11 06:17:01 +0000

    changed from extern global to bit by bit (still doesnt work)

--- Client.c
+++ Client.c
@@ -5,16 +5,48 @@
 #include <stdlib.h>
 #include "libft.h"
 #include <signal.h>
-#include "message.h"
-extern char *message;
+extern char *message = NULL;
+
+void send_signal(int PID, int bit)
+{
+	if (bit == 0)
+		kill(PID,SIGUSR1);
+	else
+		kill(PID,SIGUSR2);
+	usleep(100);
+}
+
+void send_message(int PID, char *message)
+{
+	int i = 0;
+	int j = 7;
+	while(message[i] != '\0')
+	{
+		while(j >= 0)
+		{
+			int bit = (message[i] >> j) & 1;
+			send_signal(PID,bit);
+			j--;
+		}
+		i++;
+	
+	}
+	j = 7;
+	while (j >= 0)
+    	{
+        	int bit = ('\0' >> j) & 1;
+        	send_signal(PID, bit);
+        	j--;
+	}
+
+}
 int main(int argc, char *argv[])
 {
 	if (argc == 3)
 	{
 		int PID = ft_atoi(argv[1]);
-		message = malloc(sizeof(char) * (ft_strlen(argv[2]) + 1));
-		ft_strcpy(message ,argv[2]);
-		kill(PID,SIGUSR1);
+		send_message(PID,argv[2]);
 	}
-	else{}
+	else
+		ft_printf("Unknow option for Client\n Usage: ./Client [PID] [message]\n");
 }
--- Server.c
+++ Server.c
@@ -5,21 +5,32 @@
 #include <fcntl.h>
 #include <stdlib.h>
 #include "libft.h"
-#include "message.h"
-char *message;
-void text()
+void text(int signum)
 {
-	 if (message != NULL) {
-        printf("Server received: %s\n", message);
-    } else {
-        printf("Server received: (NULL)\n");
-    }
+	int i = 0;
+	char character;
+	int gotten_bit;
+	if(signum == SIGUSR1)
+		gotten_bit = 0;
+	else if (signum == SIGUSR2)
+		gotten_bit = 1;
+	character = (character << 1) | gotten_bit;
+       	i++;
+	ft_printf("Received bit: %d, current character: %c (%d), bit count: %d\n",
+              gotten_bit, character, character, i);
+	if(i == 8)
+	{
+		ft_printf("%c",character);
+		character = 0;
+		i = 0;
+	}	
 }
 
 int main()
 {
 	int PID = getpid();
 	signal(SIGUSR1,text);
+	signal(SIGUSR2,text);
 	ft_printf("The server PID is : %d\n", PID);
 	ft_printf("To connect to this server follow this syntax.\n./client [PID] [Message]\n ");
 	while(1)
