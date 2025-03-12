ed997212c8aac9902200c45d8a3555baef41f31d
baysal <baysal@DESKTOP-H9FSBQ7.>
2025-03-12 00:40:45 +0000

    pre makefile

--- MiniTalk/Client.c
+++ MiniTalk/Client.c
@@ -1,53 +1,66 @@
-#include <unistd.h>
-#include <stdio.h>
-#include "ft_printf.h"
-#include <fcntl.h>
-#include <stdlib.h>
-#include "libft.h"
-#include <signal.h>
-extern char *message = NULL;
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Client.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: waissi <waissi@student.1337.ma>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/12 00:25:35 by waissi            #+#    #+#             */
+/*   Updated: 2025/03/12 00:28:52 by waissi           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-void send_signal(int PID, int bit)
+#include "ft_printf.h"
+#include "libft.h"
+#include <fcntl.h>
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+void	send_signal(int pid, int bit)
 {
 	if (bit == 0)
-		kill(PID,SIGUSR1);
+		kill(pid, SIGUSR1);
 	else
-		kill(PID,SIGUSR2);
+		kill(pid, SIGUSR2);
 	usleep(150);
 }
 
-void send_message(int PID, char *message)
+void	send_message(int pid, char *message)
 {
-	int i = 0;
-	int j = 7;
-	while(message[i] != '\0')
+	int	i;
+	int	j;
+	int	bit;
+	int	bit;
+
+	i = 0;
+	j = 7;
+	while (message[i] != '\0')
 	{
-		while(j >= 0)
+		while (j >= 0)
 		{
-			int bit = (message[i] >> j) & 1;
-			send_signal(PID,bit);
+			bit = (message[i] >> j) & 1;
+			send_signal(pid, bit);
 			j--;
 		}
 		i++;
-		j=7;
-	
+		j = 7;
 	}
-	/*(j = 7;
-	while (j >= 0)
-    	{
-        	int bit = ('\0' >> j) & 1;
-        	send_signal(PID, bit);
-        	j--;
-	}*/
-
 }
-int main(int argc, char *argv[])
+
+int	main(int argc, char *argv[])
 {
+	int	pid;
+
 	if (argc == 3)
 	{
-		int PID = ft_atoi(argv[1]);
-		send_message(PID,argv[2]);
+		pid = ft_atoi(argv[1]);
+		send_message(pid, argv[2]);
 	}
 	else
-		ft_printf("Unknow option for Client\n Usage: ./Client [PID] [message]\n");
+	{
+		ft_printf("Unknow option for Client\n");
+		ft_printf("Usage: ./Client [pid] [message]\n");
+	}
 }
--- MiniTalk/Server.c
+++ MiniTalk/Server.c
@@ -1,41 +1,56 @@
-#include <signal.h>
-#include <unistd.h>
-#include <stdio.h>
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Server.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: waissi <waissi@student.1337.ma>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/12 00:24:49 by waissi            #+#    #+#             */
+/*   Updated: 2025/03/12 00:24:57 by waissi           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "ft_printf.h"
-#include <fcntl.h>
-#include <stdlib.h>
 #include "libft.h"
-void text(int signum)
+#include <fcntl.h>
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+void	text(int signum)
 {
-	static int i = 0;
-	char character;
-	int gotten_bit;
-	if(signum == SIGUSR1)
+	static int	i;
+	char		character;
+	int			gotten_bit;
+
+	i = 0;
+	if (signum == SIGUSR1)
 		gotten_bit = 0;
 	else if (signum == SIGUSR2)
 		gotten_bit = 1;
 	character = (character << 1) | gotten_bit;
-       	i++;
-	if(i == 8)
+	i++;
+	if (i == 8)
 	{
-		ft_printf("%c",character);
+		ft_printf("%c", character);
 		character = 0;
 		i = 0;
-	}	
+	}
 }
 
-int main()
+int	main(void)
 {
-	int PID = getpid();
-	signal(SIGUSR1,text);
-	signal(SIGUSR2,text);
+	int	pid;
+
+	pid = getpid();
+	signal(SIGUSR1, text);
+	signal(SIGUSR2, text);
 	ft_printf("The server PID is : %d\n", PID);
-	ft_printf("To connect to this server follow this syntax.\n./client [PID] [Message]\n ");
-	while(1)
+	ft_printf("To connect to this server follow this syntax.\n");
+	ft_printf("./client [PID] [Message]\n ");
+	while (1)
 	{
 		pause();
 	}
-
-
-
 }
