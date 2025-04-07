9887b4f7b5441c3ce9ee9903fe8fbf3a29dbc956
baysal69 <aissiwassim@gmail.com>
2025-04-07 13:04:18 +0100

    b

---
+++ fractol/fractol.h
@@ -0,0 +1,36 @@
+#ifndef FRACTOL_H
+#define FRACTOL_H
+
+#include "../../minilibx-linux/mlx.h"
+#include "stdlib.h"
+#include <stdio.h>
+#include <unistd.h>
+#include <math.h>
+
+#define XMAX 600
+#define YMAX 600
+#define SCALE 250
+#define MAX_ITER 50
+
+typedef struct  s_data {
+    void    *img;
+    char    *addr;
+    int     bits_per_pixel;
+    int     line_length;
+    int     endian;
+}   t_data;
+
+typedef struct s_mlx {
+    void    *mlx_ptr;
+    void    *win_ptr;
+    t_data  img;
+    double  zoom;
+    double  offset_x;
+    double  offset_y;
+}   t_mlx;
+
+int	ft_strlen(const char *s);
+int	ft_atoi(const char *nptr);
+double	ft_atof(const char *str);
+
+#endif
---
+++ fractol/utils.c
@@ -0,0 +1,62 @@
+#include "fractol.h"
+
+int	ft_strlen(const char *s)
+{
+	size_t			i;
+
+	i = 0;
+	while (s[i] != '\0')
+		i++;
+	return (i);
+}
+
+int	ft_atoi(const char *nptr)
+{
+	long	num;
+	int		sign;
+	int		i;
+
+	i = 0;
+	sign = 1;
+	num = 0;
+	while ((nptr[i] >= '\t' && nptr[i] <= '\r') || nptr[i] == ' ')
+		nptr++;
+	if (nptr[i] == '-')
+		sign = sign * -1;
+	if (nptr[i] == '+' || nptr[i] == '-')
+		nptr++;
+	while (nptr[i] >= '0' && nptr[i] <= '9')
+	{
+		num = (num * 10) + nptr[i] - '0';
+		nptr++;
+		if (num * sign > 2147483647)
+			return (-1);
+		if (num * sign < -2147483648)
+			return (0);
+	}
+	return (num * sign);
+}
+
+double	ft_atof(const char *str)
+{
+	double	res;
+	double	res2;
+	char	*c;
+	int		len;
+
+	c = (char *)str;
+	res = (double)ft_atoi(c);
+	while (*c && *c != '.')
+		c++;
+	if (*c == '.')
+		c++;
+	res2 = (double)ft_atoi(c);
+	len = ft_strlen(c);
+	while (len--)
+		res2 /= 10;
+	if (res >= 0)
+		return (res + res2);
+	else
+		return (res + -res2);
+}
+
