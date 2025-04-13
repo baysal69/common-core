3d3758ad7e188b75ae4078d30bc4dbcd26cd9043
Wassim Aissi <waissi@c2r7p10.1337.ma>
2025-04-13 04:30:00 +0100

    ?

--- fractol/utils.c
+++ fractol/utils.c
@@ -6,7 +6,7 @@
 /*   By: waissi <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/08 12:29:51 by waissi            #+#    #+#             */
-/*   Updated: 2025/04/12 21:29:41 by waissi           ###   ########.fr       */
+/*   Updated: 2025/04/13 04:29:21 by waissi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,7 +33,7 @@ int	ft_atoi(const char *nptr)
 	num = 0;
 	while ((nptr[i] >= '\t' && nptr[i] <= '\r') || nptr[i] == ' ')
 		i++;
-	if (!(nptr[i] >= '0' && nptr[i] <= '9'))
+	if ((nptr[i] < '0' || nptr[i] > '9') && nptr[i] != '-')
 		exit(0);
 	if (nptr[i] == '-')
 		sign = sign * -1;
@@ -49,31 +49,68 @@ int	ft_atoi(const char *nptr)
 	return (num * sign);
 }
 
-double	ft_atof(const char *str)
+/*double	ft_atof(const char *str)
 {
 	double	res;
 	double	res2;
 	char	*c;
 	int		len;
+	int sign;
 
 	c = (char *)str;
-	res = (double)ft_atoi(c);
+	sign = 1;
 	while (*c && *c != '.')
 		c++;
+	if (*c == '-')
+    	{
+       		 sign *= -1;
+       		 c++;
+    	}
+   	else if (*c == '+')
+    	{
+     	  	 c++;
+    	}
+	res = (double)ft_atoi(c);
 	if (*c == '.')
-		c++;
-	res2 = (double)ft_atoi(c);
-	len = ft_strlen(c);
-	while (len--)
-		res2 /= 10;
-	while (*c)
 	{
-		if (!(*c >= '0' && *c <= '9'))
-			exit(0);
 		c++;
+		res2 = (double)ft_atoi(c);
+		len = ft_strlen(c);
+		while (len--)
+			res2 /= 10;
+		while (*c)
+		{
+			if (!(*c >= '0' && *c <= '9'))
+				exit(0);
+			c++;
+		}
 	}
-	if (res >= 0)
-		return (res + res2);
 	else
-		return (res + -res2);
+		return (sign * res);
+	return (sign * (res + res2));
+}*/
+double ft_atof(const char *str) {
+    float result = 0.0, fraction = 0.0;
+    int sign = 1, divisor = 1;
+
+    // Skip leading whitespace
+    while (*str <= 32)
+	    str++;
+    if (*str == '-' || *str == '+')
+    {
+	    if (*str == '-')
+	    	sign = *-1
+	    str++;
+    }
+
+    // Process integer part
+    while (*str >= '0' && *str <= '9') result = result * 10 + (*str++ - '0');
+    if (*str == '.') {
+        str++;
+        while (*str >= '0' && *str <= '9') {
+            fraction = fraction * 10 + (*str++ - '0');
+            divisor *= 10;
+        }
+    }
+    return sign * (result + fraction / divisor);
 }
